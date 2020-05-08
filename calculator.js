975229ae8f273587a7a573dda19e9e4ec2b12007
stepin jegan <stepinjegan@gmail.com>
2020-05-08 14:01:10 +0530

    calculator is added

---
+++ calculator.js
@@ -0,0 +1,28 @@
+let add=(a,b)=>
+{
+    let c=a+b
+    return c
+}
+let sub=(a,b)=>
+{
+    let c =a-b
+    return c
+}
+let mul=(a,b)=>
+{
+    let c=a*b
+    return c
+}
+let div=(a,b)=>
+{
+    let c=a/b
+    return c
+}
+let addition=add(5,6)
+let subtraction=sub(8,6)
+let multiplication=mul(4,3)
+let division=div(15,5)
+console.log('addition is 5 and 6 is:' , addition)
+console.log('subtration is 8 and 6 is :',subtraction)
+console.log('muliplication is 4 and 3 is:',multiplication)
+console.log('divion is 15 and 5 is :', division)
